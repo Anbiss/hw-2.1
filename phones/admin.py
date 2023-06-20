db953213c501d21f9ca9489f1b949d83b5350236
Anbiss <85112270+Anbiss@users.noreply.github.com>
2023-06-20 21:31:07 +0300

    Add files via upload

---
+++ phones/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from phones.models import Phone
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'image', 'price', 'release_date', 'lte_exists', 'slug')
+
+admin.site.register(Phone, PhoneAdmin)
